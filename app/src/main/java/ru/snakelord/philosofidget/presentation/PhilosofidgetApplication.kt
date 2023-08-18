bdb1224d205bddf5cd3644a992f9e028a0f6fb1c
snakelord757 <blackkiller1527@gmail.com>
2023-08-18 08:48:17 +0300

    Rename application class

--- app/src/main/java/ru/snakelord/philosofidget/presentation/PhilosofidgetApplication.kt
+++ app/src/main/java/ru/snakelord/philosofidget/presentation/PhilosofidgetApplication.kt
@@ -4,7 +4,7 @@ import android.app.Application
 import org.koin.core.context.startKoin
 import ru.snakelord.philosofidget.presentation.di.dependencyModule
 
-class RandomQuoteApplication : Application() {
+class PhilosofidgetApplication : Application() {
     override fun onCreate() {
         super.onCreate()
         startKoin { modules(dependencyModule) }
